d08c61f758f724857a6eea43204d2dfec39c0c12
Giovanni Sánchez <giovannis@avetti.ca>
2022-09-30 16:49:32 -0400

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'I need a Job' });
+});
+
+/* GET about page. */
+router.get('/about', function (req, res) {
+  res.render('about',
+    {
+      title: 'About this Site',
+      content: 'We will put some stuff here'
+    });
+});
+
+module.exports = router;
