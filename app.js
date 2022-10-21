73863fd4abb3d1956ae887e73eefa4b7377237f3
Giovanni Sánchez <giovannis@avetti.ca>
2022-10-21 09:46:49 -0400

    mongo connection

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ var usersRouter = require('./controllers/users');
 
 var app = express();
 
+//db conn using .env file
+if (process.env.NOVE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+  .then(
+    (res) => {
+      console.log('Connected to MongoDB')
+    }
+  ).catch(() => {
+    console.log('cannot connect to MongoDB')
+  })
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,12 +38,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
